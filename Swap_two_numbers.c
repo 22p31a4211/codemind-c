e72763d2773831abdc37a109073cae41dc9f210e
Navya bindu dwarampudi <123063423+22p31a4211@users.noreply.github.com>
2023-01-28 22:32:34 +0530

    #include<stdio.h> int main() { 	int  first,second,temp; 	scanf("%d%d",&first,&second); temp=first; first=second; second=temp; 	printf("%d ",first); 	printf("%d",second); }

---
+++ Swap_two_numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int  first,second,temp;
+	scanf("%d%d",&first,&second);
+temp=first;
+first=second;
+second=temp;
+	printf("%d
+",first);
+	printf("%d",second);
+}

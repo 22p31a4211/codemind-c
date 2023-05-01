0522620e0d7de56ea43285820b535b3f721ffa37
Navya bindu dwarampudi <123063423+22p31a4211@users.noreply.github.com>
2023-05-01 17:19:42 +0530

    #include <stdio.h>
    int main() {
        int principal, rate, time, interest;
    
        scanf("%d", &principal);
    
        scanf("%d", &rate);
    
        scanf("%d", &time);
    
        interest = (principal * rate * time) / 100;
    
        printf("%d", interest);
    
        return 0;
    }

---
+++ SIMPLE_INTEREST.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int principal, rate, time, interest;
+
+    scanf("%d", &principal);
+
+    scanf("%d", &rate);
+
+    scanf("%d", &time);
+
+    interest = (principal * rate * time) / 100;
+
+    printf("%d", interest);
+
+    return 0;
+}
